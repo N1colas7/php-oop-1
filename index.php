4b079dbeb116cc7cc7c1995bd04905d347d306b4
N1colas7 <nicolasnd9897@gmail.com>
2023-02-03 16:59:05 +0100

    create a class and insert variable instance

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+class Movie{
+    public $nazionalita;
+    public $genere;
+}
+
+$movie_1 = new Movie();
+$movie_1->genere = "Horror";
+$movie_1->nazionalita = "USA"; 
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +20,9 @@
     <title>PHP-OOP-1</title>
 </head>
 <body>
-    
+    <pre>
+        <?php var_dump($movie_1);?>
+    </pre>
+        <?php echo $movie_1->genere; ?>
 </body>
 </html>
