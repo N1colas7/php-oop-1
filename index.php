e6de0a6fdd8b163e2a98aa44dbbf5c1db6e04dd4
N1colas7 <nicolasnd9897@gmail.com>
2023-02-03 17:30:42 +0100

    push to fix

--- index.php
+++ index.php
@@ -3,15 +3,29 @@
 class Movie{
     public $nazionalita;
     public $genere;
+    public $durata = 130;
+    
+    public function setDurata($lunghezza) {
+        if($lunghezza < 100 ){
+            $this->lunghezza = "Film di breve durata";
+        }
+    }
+     
+    public function getDurata() {
+        return $this->lunghezza;
+        }
 
-    function __construct($_genere) {
+    function __construct($_genere, $_nazionalita) {
         $this->genere = $_genere;
+        $this->nazionalita = $_nazionalita;
+
     }
 }
 
-$movie_1 = new Movie("Horror");
+$movie_1 = new Movie("Horror","USA");
 //echo $movie_1->genere;
-$movie_1->nazionalita = "USA";  
+$movie_1->setDurata("130");
+$durata_movie_1 = $movie_1->getDurata();
 
 ?>
 
@@ -29,5 +43,6 @@ $movie_1->nazionalita = "USA";
     </pre>
         <p>Il genere del film è :<?php echo $movie_1->genere; ?></p>
         <p>Il film è stato fatto in :<?php echo $movie_1->nazionalita; ?></p>
+        <p><?php echo $movie_1->getDurata() ?></p>
 </body>
 </html>
